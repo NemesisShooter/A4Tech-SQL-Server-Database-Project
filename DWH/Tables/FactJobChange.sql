3ed35334117101d2c85794cd9f1fd4ef813ca1f3
NemesisShooter <grekdarkness@gmail.com>
2018-06-07 18:20:43 +0300

    Created Post-Deployment script, updated view_job_information

--- DWH/Tables/FactJobChange.sql
+++ DWH/Tables/FactJobChange.sql
@@ -2,9 +2,8 @@
     [IdDimEmployee] INT   NULL,
     [IdDimJobs]     INT   NULL,
     [IdDimDate]     INT   NULL,
-    [FactMoney]     MONEY NULL,
-    CONSTRAINT [FK_DimDate] FOREIGN KEY ([IdDimDate]) REFERENCES [DWH].[DimDate] ([DateKey]),
-    CONSTRAINT [FK_DimEmployee] FOREIGN KEY ([IdDimEmployee]) REFERENCES [DWH].[DimEmployee] ([IdDimEmployee]),
-    CONSTRAINT [FK_DimJobs] FOREIGN KEY ([IdDimJobs]) REFERENCES [DWH].[DimJobs] ([IdDimJobs])
+    [FactMoney]     MONEY NULL
 );
+
+
 
--- DWH/Views/vw_employee_job_information.sql
+++ DWH/Views/vw_employee_job_information.sql
@@ -1,6 +1,7 @@
 ﻿
 
 
+
 CREATE VIEW [DWH].[vw_employee_job_information] AS
 
 SELECT 
@@ -9,24 +10,24 @@ SELECT
 	LastName,
 	JobTitle,
 	FactMoney AS Salary,
-	CASE WHEN DWH.FactTable.IdDimDate = MAX(IdDimDate)OVER(PARTITION BY DWH.DimEmployee.BusinessEntityID) THEN 'Present' ELSE 'Past' END AS CurrentJob,
-	DWH.FactTable.IdDimDate
-FROM	DWH.FactTable
+	CASE WHEN DWH.FactJobChange.IdDimDate = MAX(IdDimDate)OVER(PARTITION BY DWH.DimEmployee.BusinessEntityID) THEN 'Present' ELSE 'Past' END AS CurrentJob,
+	DWH.FactJobChange.IdDimDate
+FROM	DWH.FactJobChange
 INNER JOIN DWH.DimEmployee
-	ON	DWH.DimEmployee.IdDimEmployee = DWH.FactTable.IdDimEmployee
+	ON	DWH.DimEmployee.IdDimEmployee = DWH.FactJobChange.IdDimEmployee
 INNER JOIN DWH.DimJobs
-	ON	DWH.DimJobs.IdDimJobs = DWH.FactTable.IdDimJobs
+	ON	DWH.DimJobs.IdDimJobs = DWH.FactJobChange.IdDimJobs
 INNER JOIN DWH.DimDate
-	ON DWH.DimDate.DateKey = DWH.FactTable.IdDimDate
+	ON DWH.DimDate.DateKey = DWH.FactJobChange.IdDimDate
 WHERE DWH.DimEmployee.BusinessEntityID IN 
 (
 SELECT
 	BusinessEntityID
-FROM	DWH.FactTable
+FROM	DWH.FactJobChange
 INNER JOIN DWH.DimEmployee
-	ON	DWH.DimEmployee.IdDimEmployee = DWH.FactTable.IdDimEmployee
+	ON	DWH.DimEmployee.IdDimEmployee = DWH.FactJobChange.IdDimEmployee
 INNER JOIN DWH.DimJobs
-	ON	DWH.DimJobs.IdDimJobs = DWH.FactTable.IdDimJobs
+	ON	DWH.DimJobs.IdDimJobs = DWH.FactJobChange.IdDimJobs
 GROUP BY DWH.DimEmployee.BusinessEntityID
-HAVING COUNT(DWH.FactTable.IdDimDate) >= 2
+HAVING COUNT(DWH.FactJobChange.IdDimDate) >= 2
 )
